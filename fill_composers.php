74078c490de6e80e2dcab081e973719548d68b92
Nosferius <nosferius@gmail.com>
2016-01-15 23:06:21 +0100

    First New edition (2) With some css

--- fill_composers.php
+++ fill_composers.php
@@ -46,3 +46,37 @@ foreach($data as $ComposerLastname)
     $form->add();
     echo "Added: ".$ComposerLastname."<br>";
 }
+
+/*
+                    <option value="1"> Albeniz </option>
+                    <option value="Bach"> Bach </option>
+                    <option value="Bartok"> Bartok </option>
+                    <option value="Beethoven"> Beethoven </option>
+                    <option value="Busoni"> Busoni </option>
+                    <option value="Brahms"> Brahms </option>
+                    <option value="Chopin"> Chopin </option>
+                    <option value="Debussy"> Debussy </option>
+                    <option value="Franck"> Franck </option>
+                    <option value="Haydn"> Haydn </option>
+                    <option value="Joplin"> Joplin </option>
+                    <option value="Mendelssohn"> Mendelssohn </option>
+                    <option value="Mozart"> Mozart </option>
+                    <option value="Mussorgsky"> Mussorgsky </option>
+                    <option value="Paderewski"> Paderewski </option>
+                    <option value="Prokofiev"> Prokofiev </option>
+                    <option value="Rachmaninoff"> Rachmaninoff </option>
+                    <option value="Ravel"> Ravel </option>
+                    <option value="Rimski-Korsakov"> Rimski-Korsakov </option>
+                    <option value="Rubinstein"> Rubinstein </option>
+                    <option value="Saint-Saëns"> Saint-Saëns </option>
+                    <option value="Satie"> Satie </option>
+                    <option value="Scarlatti"> Scarlatti </option>
+                    <option value="Schubert"> Schubert </option>
+                    <option value="Schumann"> Schumann </option>
+                    <option value="Scriabin"> Scriabin </option>
+                    <option value="Shostakovich"> Shostakovich </option>
+                    <option value="Sibelius"> Sibelius </option>
+                    <option value="Tchaikovsky"> Tchaikovsky </option>
+                    <option value="Liszt"> Liszt </option> -->
+
+*/
